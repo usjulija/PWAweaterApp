6d3d5741973f3172d602e96cf02941cef9e4bf18
Julia <us.julija@gmail.com>
2018-09-20 00:37:28 +0300

    sw

--- sw.js
+++ sw.js
@@ -9,8 +9,33 @@ self.addEventListener('install', function(event) {
         return cache.addAll([
           '/',
           '/css/styles.css',
+          '/css/mixins.css',
+          '/images/01d.svg',
+          '/images/01n.svg',
+          '/images/02d.svg',
+          '/images/02n.svg',
+          '/images/03d.svg',
+          '/images/03n.svg',
+          '/images/04d.svg',
+          '/images/04n.svg',
+          '/images/09d.svg',
+          '/images/09n.svg',
+          '/images/10d.svg',
+          '/images/10n.svg',
+          '/images/11d.svg',
+          '/images/11n.svg',
+          '/images/13d.svg',
+          '/images/13n.svg',
+          '/images/50d.svg',
+          '/images/50n.svg',
+          '/images/celsius.svg',
+          '/images/error.svg',
+          '/images/fahrenheit.svg',
+          '/images/umbrella.svg',
           '/js/app.js',
-          '/index.html'
+          '/js/localforage-1.4.0.js',
+          '/index.html',
+          'https://fonts.googleapis.com/icon?family=Material+Icons'
         ]);
       })
       .catch(function() {
